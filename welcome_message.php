e85c217195772b40945ca345796fd999f5f60aef
nishamane2277 <125238219+nishamane2277@users.noreply.github.com>
2023-04-25 09:50:34 +0530

    welcome_message.php

---
+++ welcome_message.php
@@ -0,0 +1,174 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Navigation Menu Demo</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css"> 
+</head>
+<body>
+    <style>
+        @import url('https://fonts.googleapis.com/css2?family=Quicksand:wght@300;400;500;600;700&display=swap');
+
+body{
+    font-family: 'Quicksand', sans-serif;
+}
+
+a{
+    text-decoration: black;
+}
+
+li{
+    list-style-type: none;
+}
+
+header{
+    
+    align-items: right;
+    padding: 0px;
+    background-color: White;
+    
+}
+
+
+nav ul{
+    display: flex;
+    color:black;
+
+}
+
+nav ul li{
+
+      margin-right: 0px;
+}
+nav ul li a{
+    color:black;
+    padding: 10px 20px;
+}
+
+nav ul li a:hover{
+    color:black;
+}
+
+.cta{
+    background-color: ;
+    color:black ;
+}
+
+.cta:hover{
+    background-color: ;
+    color: black;
+}
+
+
+    </style>
+    <style>
+        body{
+
+
+           font-family: 'Quicksand', sans-serif;
+}
+a{
+    text-decoration: none;
+}
+
+li{
+    list-style-type: none;
+
+}
+container{
+    
+        align-items: left;
+        padding: 0px;
+        background-color: White;
+    
+}
+
+</style>
+
+    </header>
+    
+        <nav>
+            <ul>
+                <b><li><a href="#">My account</a></li></b>
+                <b><li><a href="#">Login / Register</a></li></b>
+                <b><li><a href="#">Orders</a></li></b>
+                <b><li><a href="#">Track Orders</a></li></b>
+                <b><li><a href="#"class="cta">Wishlist</a></li></b>
+            </ul>
+            <hr>  <h1>  </h1></hr>
+</nav>
+    <nav>
+        
+                    <ul>
+                       <a><img src=/images/cropped-MensFyt-1-1.png width=100px height=60px></a>
+                       <li><a href="#"></a></li>
+                       <li><a href="#"></a></li>
+                       <li><a href="#"></a></li>
+                       <li><a href="#"></a></li>
+                       <li><a href="#"></a></li>
+                       <li><a href="#"></a></li>
+                       <li><a href="#"></a></li>
+                       <li><a href="#"></a></li>
+                       <li><a href="#"></a></li>
+                       <li><a href="#"></a></li>
+                       <li><a href="#"></a></li>
+                       <li><a href="#"></a></li>
+                       <li><a href="#"></a></li>
+                       <li><a href="#"></a></li>
+                       <li><a href="#"></a></li>
+                       <li><a href="#"></a></li>
+                       <li><a href="#"></a></li>
+                    
+                        
+    
+    
+                       <b><li><a href="#">HOME</a></li></b>
+                       <b> <li><a href="#">SHOP</a></li></b>
+                       <b><li><a href="#">BLOG</a></li></b>
+                       <b><li><a href="#">ABOUT US</a></li></b>
+                       <b><li><a href="#" class="container">CONTACT</a></li></b>
+                       <li><a><img src=/images/shopping-bag.png width=30px height=30px> </a></li>
+
+                       <li><a><img src=/images/magnifying-glass.png width=20px height=30px> </a></li>
+
+                    </ul>
+                       <img src=/images/Screenshot_2023-02-28_at_8.39.34_AM.png width=1520 height=550>
+
+
+
+
+                       <img src=/images/Screenshot_2023-02-28_at_8.40.28_AM.png width=1520 height=550>
+
+
+
+
+                       <img src=/images/Screenshot_2023-02-28_at_8.41.03_AM.png width=1520 height=200>
+
+
+
+
+                       <img src=/images/Screenshot_2023-02-28_at_8.41.54_AM.png width=1520 height=550>
+
+
+
+
+
+
+
+                       
+
+                       <img src=/images/Screenshot_2023-02-28_at_8.42.27_AM.png width=1520 height=300>
+
+
+
+
+
+
+
+                      <img src=/images/Screenshot_2023-02-28_at_8.42.55_AM.png width=1520 height=550>
+
+</nav>              
+</footer>
+</body>
+</html>
